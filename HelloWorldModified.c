c21740fe665e5af076815e57f8dc3e7a55db89e3
Emeara <emearamiller@gmail.com>
2016-10-26 20:46:54 -0500

    Added World File!

---
+++ HelloWorldModified.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int x = 4;
+    int y = 5;
+    printf("Hello World\n");
+    printf("%s%d\n", "Added Value: ", x + y);
+    if((x + y) > 0)
+    {
+        printf("%s%d%s\n", "The value ", (x + y), " is larger than 0");
+    }
+    return 0;
+}
