cbd36eea60786fbe67af64f4d679af2712c92859
pontsci <t89pepper@gmail.com>
2016-10-27 15:07:12 -0500

    begin implementing loops for menu system.

--- HelloWorldModified.c
+++ HelloWorldModified.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
+
+int reqInput(int *x, int *y);
+int printMenu();
+
 int main()
 {
-    int x = 0;
-    int y = 0;
+    int *x;
+    int *y;
+    *x = 0;
+    *y = 0;
+    int exit = 0;
 
     printf("Hello World!!\n");
     
-    printf("%s", "Enter first value: ");
-    scanf("%d", &x);
-    printf("%s", "Enter second value: ");
-    scanf("%d", &y);
-
+    while(exit == 0)
+    {
+        
+    }
     printf("%s%d\n", "Added Value: ", x + y);
     if((x + y) > 0)
     {
@@ -22,5 +28,14 @@ int main()
     }
     else
         printf("%s%d%s\n", "The value ", (x + y), "is smaller than 0.");
+    return 0;
+}
+
+int reqInput(int *x, int *y)
+{
+    printf("%s", "Enter first value: ");
+    scanf("%d", &x);
+    printf("%s", "Enter second value: ");
+    scanf("%d", &y);
     return 0;
 }
